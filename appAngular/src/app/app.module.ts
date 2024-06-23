24b1ff3cea52278f9e04d0d03dbcd8bd52b3f875
MackD8 <45779480+Mack8@users.noreply.github.com>
2024-06-22 23:50:19 -0600

    Angular

--- appAngular/src/app/app.module.ts
+++ appAngular/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { VideojuegoModule } from './videojuego/videojuego.module';
+import { OrdenModule } from './orden/orden.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +18,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    UserModule,
+    VideojuegoModule,
+    OrdenModule,
+    AppRoutingModule,
+    
   ],
   providers: [
     provideAnimationsAsync()
