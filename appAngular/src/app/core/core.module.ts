126150925d9ceda06febf2da6c6347556153a482
Mack8 <marco-daniel08@hotmail.com>
2024-06-26 17:03:12 -0600

    Imagenes

--- appAngular/src/app/core/core.module.ts
+++ appAngular/src/app/core/core.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
